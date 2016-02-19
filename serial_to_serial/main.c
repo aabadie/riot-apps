0403f33d662c92afb328d304477b4b5199b62623
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-02-19 14:55:42 +0100

    thread safe implementation fo the uart to uart communication

--- serial_to_serial/main.c
+++ serial_to_serial/main.c
@@ -1,8 +1,8 @@
 /*
- * Copyright (C) 2014 Freie Universität Berlin
+ * Copyright (C) 2016 Inria
  *
  * This file is subject to the terms and conditions of the GNU Lesser
- * General Public License v2.1. See the file LICENSE in the top level
+ * General Public License v3. See the file LICENSE in the top level
  * directory for more details.
  */
 
@@ -20,54 +20,64 @@
 #include <stdlib.h>
 
 #include "board.h"
-#include "ringbuffer.h"
+#include "msg.h"
+#include "thread.h"
 #include "periph/uart.h"
 
-#define UART_BUFSIZE        (128U)
 #define PC_UART 0
 #define BT_UART 1
 #define BAUDRATE (9600U)
 
-typedef struct {
-  char rx_mem[UART_BUFSIZE];
-  ringbuffer_t rx_buf;
-} uart_ctx_t;
+static kernel_pid_t idle_thread_pid;
 
-static uart_ctx_t ctx[UART_NUMOF];
-
-static void rx_cb(void *arg, char data)
+static void rx_cb(void *uart, char c)
 {
-  ringbuffer_t *buf = (ringbuffer_t *)arg;
-  ringbuffer_add_one(buf, data);
+  /* A character was received on an UART interface and triggered
+  this callback via an interruption, we forward it via a message
+  to the idle thread. */
+  msg_t msg;
+  msg.type = (int)uart;
+  msg.content.value = (uint32_t)c;
+  msg_send(&msg, idle_thread_pid);
 }
 
 int main(void)
 {
-  puts("Connection between 2 UART interfaces");
-  puts("=====================================");
+  printf("Connection between 2 UART interfaces\n");
+  printf("====================================\n");
   
-  /* initialize ringbuffers and serial devices */
+  /* Initialize serial devices */
   /* UART0 : PC terminal */
-  ringbuffer_init(&(ctx[PC_UART].rx_buf), ctx[PC_UART].rx_mem, UART_BUFSIZE);
-  uart_init(PC_UART, BAUDRATE, rx_cb, (void *)&(ctx[PC_UART].rx_buf));
+  uart_init(PC_UART, BAUDRATE, rx_cb, (void *)PC_UART);
 	
   /* UART2 : BT terminal */
-  ringbuffer_init(&(ctx[BT_UART].rx_buf), ctx[BT_UART].rx_mem, UART_BUFSIZE);
-  uart_init(BT_UART, BAUDRATE, rx_cb, (void *)&(ctx[BT_UART].rx_buf));
+  uart_init(BT_UART, BAUDRATE, rx_cb, (void *)BT_UART);
 
+  /* Get Idle thread pid */
+  idle_thread_pid = thread_getpid();
+  
+  /* Local variable containing the message exchanged between 
+  the 2 UART interfaces */
+  msg_t msg;
   for (;;) {
-	while (ctx[BT_UART].rx_buf.avail) {
-	  // We have data coming from UART2 => simply print on PC terminal
-	  int data = ringbuffer_get_one(&(ctx[BT_UART].rx_buf));
-	  printf("%c", (char)data);
-	}
-
-	while (ctx[PC_UART].rx_buf.avail) {
-	  // We have data printed on the PC terminal => forward on UART2
-	  int data = ringbuffer_get_one(&(ctx[PC_UART].rx_buf));
-	  uart_write(BT_UART, (uint8_t*)&data, 1);
+	/* Next line blocks the loop until a message is received on the idle thread */
+	msg_receive(&msg);
+	int uart = msg.type; /* 'type' contains the UART interface number */
+	switch (uart) {
+	case PC_UART:
+	  /* Forward message to BT UART */
+	  uart_write(BT_UART, (uint8_t*)&(msg.content.value), 1);
+	  break;
+	case BT_UART:
+	  /* Print message on PC terminal */
+	  printf("%c", (char)msg.content.value);
+	  break;
+	default:
+	  /* Should not happen */
+	  printf("Unknown UART interface %i", uart);
+	  break;
 	}
   }
-	
+
   return 0;
 }
