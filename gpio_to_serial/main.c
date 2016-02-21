5f0d46f6703e975687546f4290a071bfe44aea3c
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-02-21 16:40:38 +0100

    working version with on board samr21-xpro led and button

--- gpio_to_serial/main.c
+++ gpio_to_serial/main.c
@@ -21,69 +21,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "shell.h"
+#include "board.h"
 #include "msg.h"
 #include "thread.h"
 #include "periph/gpio.h"
 #include "periph/uart.h"
 
-#define GPIO_BUTTON_PORT 10
-#define GPIO_BUTTON_PIN 10
-
-#define GPIO_LED_PORT 11
-#define GPIO_LED_PIN 11
-
 #define UART_INTERFACE 0
 #define BAUDRATE (9600U)
 
 static kernel_pid_t idle_thread_pid;
-static bool status = 0;
+
+static void uart_cb(void *dev, char data)
+{   
+    printf("\rUART Callback\n");
+    gpio_toggle(LED_GPIO);
+    msg_t msg;
+    msg.content.value = (uint32_t)(NULL);
+    msg_send(&msg, idle_thread_pid);
+}
 
 static void gpio_cb(void *pin)
 {
-    gpio_toggle(GPIO_PIN(GPIO_LED_PORT, GPIO_LED_PIN));
-    printf("INT: external interrupt from pin %i\n", (int)pin);
+    printf("\rGPIO Callback\n");
+    gpio_toggle(LED_GPIO);
     msg_t msg;
-    msg.content.value = (uint32_t)(gpio_read(GPIO_PIN(GPIO_LED_PORT, GPIO_LED_PIN)));
+    msg.content.value = (uint32_t)(NULL);
     msg_send(&msg, idle_thread_pid);
 }
 
 int main(void)
 {
-    puts("GPIO to UART sample application\n");
+    puts("\rGPIO to UART sample application\n");
 
-    if (gpio_init(GPIO_PIN(GPIO_LED_PORT, GPIO_LED_PIN),
-		  GPIO_DIR_OUT, GPIO_PULLDOWN) < 0) {
-        printf("Error while initializing LED on PORT_%i.%i as output\n",
-	       GPIO_LED_PORT, GPIO_LED_PIN);
+    if (gpio_init(LED_GPIO, GPIO_DIR_OUT, GPIO_NOPULL) < 0) {
+        puts("\rError while initializing LED GPIO as output\n");
         return 1;
     }
-    printf("LED on PORT_%i.%i initialized successfully as output\n",
-	   GPIO_LED_PORT, GPIO_LED_PIN);
+    puts("\rLED GPIO initialized successfully as output\n");
+    /* Shutdown on board LED */
+    gpio_set(LED_GPIO);
     
-    if (gpio_init_int(GPIO_PIN(GPIO_BUTTON_PORT, GPIO_BUTTON_PIN),
-		      GPIO_PULLDOWN, GPIO_RISING, gpio_cb,
-		      (void *)GPIO_BUTTON_PIN) < 0) {
-        printf("Error while initializing  PORT_%i.%02i as external interrupt\n",
-               GPIO_BUTTON_PORT, GPIO_BUTTON_PIN);
+    if (gpio_init_int(BUTTON_GPIO, GPIO_PULLUP, GPIO_RISING, gpio_cb,
+		      (void *)BUTTON_GPIO) < 0) {
+        puts("\rError while initializing BUTTON GPIO as external interrupt\n");
         return 1;
     }
-    printf("PORT_%i.%02i initialized successful as external interrupt\n",
-           GPIO_BUTTON_PORT, GPIO_BUTTON_PIN);
+    puts("\rBUTTON GPIO initialized successfully as external interrupt\n");
 
+    
     /* Initialize UART interface */
-    uart_init(UART_INTERFACE, BAUDRATE, NULL, (void *)NULL);
+    if (uart_init(UART_INTERFACE, BAUDRATE, uart_cb, (void *)NULL) < 0) {
+	puts("\rError while initializing UART interface\n");
+	return 1;
+    }
+    puts("\rUART interface initialized successfully\n");
+    
     
     /* Get Idle thread pid */
     idle_thread_pid = thread_getpid();
-
     msg_t msg;
+    bool status;
     for (;;) {
-	msg_receive(&msg); /* This line blocks the loop until a message is received. */
-	bool status = (bool)msg.content.value;
-	uart_write(UART_INTERFACE,
-		   (uint8_t*)strcat("Button pressed, LED is now ",
-				    status? "ON" : "OFF"), 1);
+	msg_receive(&msg); /* This line blocks the loop until a message is 
+			      received. */
+	status = gpio_read(LED_GPIO);
+	printf("Message received, LED is %s", status? "ON" : "OFF");
     }
     
     return 0;
