0f9f32c418cadbbffe399adf7323aa79f862dabe
Alexandre Abadie <alexandre.abadie@inria.fr>
2016-02-24 17:54:08 +0100

    add an example tool for configuring a bluetooth module from a RIOT-OS shell

---
+++ bt_config/README.md
@@ -0,0 +1,71 @@
+## RIOT-OS based tool for bluetooth module configuration
+
+Bluetooth modules available on the market generally needs to be configured
+before any usage. Indeed, ones has to change it's public name, set its mode to
+slave or master, give a password and change its serial speed.
+The goal is this sample application is to show how to use the shell module of
+RIOT-OS to achieve this bluetooth configuration initial steps.
+
+Note that you need at least 2 UART interfaces on your board to test it :
+* UART0: connected to a PC USB port (serial debug interface).
+* UART1: connected to the UART of the bluetooth module.
+
+3 concepts of RIOT are illustrated here:
+* [Shell](http://riot-os.org/api/group__sys__shell.html)
+* [IPC](http://riot-os.org/api/group__core__msg.html#details)
+* [UART](http://riot-os.org/api/group__drivers__periph__uart.html#details)
+* [IRQ](http://riot-os.org/api/group__core__irq.html#details)
+
+
+### Hardware setup
+
+This application has been tested with a [bluetooth 42N based module from
+Sparkfun](https://www.sparkfun.com/products/12577) and the boards mentioned in
+the top-level [README](https://github.com/aabadie/riot-apps#sample-applications-using-riot-os) of this repository.
+
+You can find more information on a RN42 module configuration and especially the commands
+required
+[here](https://eewiki.net/display/Wireless/Getting+Started+with+RN42+Bluetooth+Module#GettingStartedwithRN42BluetoothModule-RN42UARTCommands)
+The HC-05 module has also been tested and should work as
+well. [Here](http://www.linotux.ch/arduino/HC-0305_serial_module_AT_commamd_set_201104_revised.pdf)
+is a document explaining the set of commands to configure it.
+
+<img src="https://github.com/aabadie/riot-apps/blob/master/serial_to_serial/docs/uart_2_uart_arduino_due_bb.png" width="400">
+
+### Using this application
+
+* Ensure you followed the
+[prerequisites](https://github.com/aabadie/riot-apps#prerequisites) first
+* Follow the setup displayed above and plug the board to an USB port of your computer
+* Build and flash:
+```bash
+$ cd ~/work/i2c_temperature
+$ make RIOTBASE=~/work/RIOT BOARD=arduino-due flash
+```
+* Start a serial terminal application on your PC (assuming the board is
+visible on `/dev/ttyACM0`)
+```bash
+gtkterm -p /dev/ttyACM0 -s 115200 -b 8
+```
+* In the RIOT-OS shell, type `help` to list the available commands.
+* By default the RN42 works at 115200 bps and the configuration mode is launched
+with the `$$$` string. Simply use the following command to switch the module in
+config mode:
+```bash
+> start 115200 $$$
+```
+* Then, you can list and change some settings, for example:
+```bash
+# show the list of settings:
+> send D
+# set a new name:
+> send SN,<new_name>
+```
+* Once done, the module needs to be rebooted (some settings require this):
+```bash
+> send R,1
+```
+* If you just want to quit the the configuration mode:
+```bash
+> send ---
+```
---
+++ bt_config/main.c
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2016 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v3. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @{
+ * @brief Uart to uart communication program
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "board.h"
+#include "shell.h"
+#include "msg.h"
+#include "thread.h"
+#include "periph/uart.h"
+
+#define BT_UART 1
+
+#define SHELL_BUFSIZE       (128U)
+
+#define PRINTER_PRIO        (THREAD_PRIORITY_MAIN - 1)
+#define PRINTER_TYPE        (0xabcd)
+
+static kernel_pid_t printer_pid;
+static char printer_stack[THREAD_STACKSIZE_MAIN];
+
+static void rx_cb(void *uart, char c)
+{
+    /* A character was received on an UART interface and triggered
+       this callback through an interruption, we forward it via a message
+       to the idle thread. */
+    msg_t msg;
+    msg.type = (int)uart;
+    msg.content.value = (uint32_t)c;
+    msg_send(&msg, printer_pid);
+}
+
+static void *printer(void *arg)
+{
+    (void)arg;
+    
+    /* Local variable containing the message exchanged between 
+       the 2 UART interfaces */
+    msg_t msg;
+    msg_t msg_queue[8];
+    msg_init_queue(msg_queue, 8);
+    for (;;) {
+	/* Next line blocks the loop until a message is received on the idle 
+	   thread */
+	msg_receive(&msg);
+	printf("%c", (char)msg.content.value);
+    }
+
+    /* this should never be reached */
+    return NULL;
+}
+
+static int cmd_init(int argc, char **argv)
+{
+    int res;
+    uint32_t baud;
+
+    if (argc < 3) {
+        printf("usage: %s <baudrate> <init command>\n", argv[0]);
+        return 1;
+    }
+
+    baud = (uint32_t)atoi(argv[1]);
+
+    /* initialize UART */
+    res = uart_init(BT_UART, baud, rx_cb, (void *)BT_UART);
+    if (res == -1) {
+        printf("Error: Given baudrate (%u) not possible\n", (unsigned int)baud);
+        return 1;
+    }
+    else if (res < -1) {
+        puts("Error: Unable to initialize bluetooth device\n");
+        return 1;
+    }
+    printf("Successfully initialized bluetooth device\r\n");
+
+    uart_write(BT_UART, (uint8_t *)argv[2], strlen(argv[2]));
+
+    return 0;
+}
+
+static int cmd_send(int argc, char **argv)
+{   
+    if (argc < 2) {
+        printf("usage: %s <command>\n", argv[0]);
+        return 1;
+    }
+    
+    uart_write(BT_UART, (uint8_t *)strcat(argv[1], "\r\n"), strlen(argv[1]) + 2);
+    return 0;
+}
+
+static const shell_command_t shell_commands[] = {
+    { "init", "\r init: Start bluetooth module configuration mode\n", cmd_init },
+    { "send", "\r send: Send a command to the bluetooth module\r\n", cmd_send },
+    { NULL, NULL, NULL }
+};
+
+int main(void)
+{
+    printf("\r\nBluetooth module configuration tool\r\n");
+    printf("===================================\r\n");
+
+    /* start the printer thread */
+    printer_pid = thread_create(printer_stack, sizeof(printer_stack),
+                                PRINTER_PRIO, 0, printer, NULL, "printer");
+
+    /* run the shell */
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+
+    return 0;
+}
